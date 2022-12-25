02de52612a3c77c655c23539809cf45fc43892d9
Wilson Cusack <cusackw@gmail.com>
2022-12-25 07:31:07 -0600

    clarify ENS names can be used

--- src/components/ConnectedHome.tsx
+++ src/components/ConnectedHome.tsx
@@ -1,13 +1,13 @@
 import { ethers } from "ethers"
 import { useCallback, useState } from "react"
-import { useAccount, useSigner } from "wagmi"
-import { NounishChristmasNFT__factory, NounishWhiteElephant__factory } from "../../types/generated/abis"
-import { provider } from "../pages/games/[gameID]"
+import {  useSigner } from "wagmi"
+import { NounishWhiteElephant__factory } from "../../types/generated/abis"
 
 export function ConnectedHome() {
     const { data: signer, isError, isLoading } = useSigner()
     const [waitingForTx, setWaitingForTx] = useState(false)
     const [participants, setParticipants] = useState('')
+
 
     const handleClick = useCallback(async () => {
         if (!signer) return
@@ -17,6 +17,10 @@ export function ConnectedHome() {
             participants: participants.split(',').map((c) => c.trim()),
             nonce: ethers.BigNumber.from(Date.now().toString())
         }
+
+        // game.participants = await Promise.all(
+        //     game.participants.map(async (c) => await provider.resolveName(c) || c)
+        // )
 
         const id = await contract.gameID(game);
         
@@ -31,7 +35,7 @@ export function ConnectedHome() {
 
     return(
         <div>
-            <p> Enter a comma seperated list of the addresses you want to play white elephant with. </p>
+            <p> Enter a comma seperated list of the addresses or ENS names you want to play white elephant with. </p>
             <p> You will pay 0.01 ETH per participant and, by the end, everyone will get a unique NFT. </p>
             <textarea placeholder='participants' onChange={(e) => setParticipants(e.target.value) }/>
             <br/>
